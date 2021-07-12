e350da64ab853a5f686286ad0c189d7ebd6d5361
futuretea <Hang.Yu@suse.com>
2021-07-12 16:41:16 +0800

    update provider

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -16,33 +16,31 @@ import (
 	"github.com/harvester/terraform-provider-harvester/pkg/constants"
 )
 
-func New(version string) func() *schema.Provider {
-	return func() *schema.Provider {
-		p := &schema.Provider{
-			Schema: map[string]*schema.Schema{
-				constants.FiledProviderKubeConfig: {
-					Type:        schema.TypeString,
-					Optional:    true,
-					Default:     "",
-					Description: "harvester kubeconfig",
-				},
+func Provider() *schema.Provider {
+	p := &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			constants.FiledProviderKubeConfig: {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "",
+				Description: "harvester kubeconfig",
 			},
-			DataSourcesMap: map[string]*schema.Resource{},
-			ResourcesMap: map[string]*schema.Resource{
-				constants.ResourceTypeImage:          image.ResourceImage(),
-				constants.ResourceTypeKeyPair:        keypair.ResourceKeypair(),
-				constants.ResourceTypeNetwork:        network.ResourceNetwork(),
-				constants.ResourceTypeVirtualMachine: virtualmachine.ResourceVirtualMachine(),
-				constants.ResourceTypeVolume:         volume.ResourceVolume(),
-				constants.ResourceTypeClusterNetwork: clusternetwork.ResourceClusterNetwork(),
-			},
-		}
-		p.ConfigureContextFunc = configure(version, p)
-		return p
+		},
+		DataSourcesMap: map[string]*schema.Resource{},
+		ResourcesMap: map[string]*schema.Resource{
+			constants.ResourceTypeImage:          image.ResourceImage(),
+			constants.ResourceTypeKeyPair:        keypair.ResourceKeypair(),
+			constants.ResourceTypeNetwork:        network.ResourceNetwork(),
+			constants.ResourceTypeVirtualMachine: virtualmachine.ResourceVirtualMachine(),
+			constants.ResourceTypeVolume:         volume.ResourceVolume(),
+			constants.ResourceTypeClusterNetwork: clusternetwork.ResourceClusterNetwork(),
+		},
 	}
+	p.ConfigureContextFunc = configure(p)
+	return p
 }
 
-func configure(version string, p *schema.Provider) schema.ConfigureContextFunc {
+func configure(p *schema.Provider) schema.ConfigureContextFunc {
 	return func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
 		kubeConfig := d.Get(constants.FiledProviderKubeConfig).(string)
 		c, err := client.NewClient(kubeConfig)
