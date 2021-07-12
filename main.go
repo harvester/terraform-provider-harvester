e80d58580d9c5785c7362a65b70bdec0cafa1130
futuretea <Hang.Yu@suse.com>
2021-07-12 16:41:16 +0800

    update provider

--- main.go
+++ main.go
@@ -13,16 +13,12 @@ import (
 //go:generate terraform fmt -recursive ./examples/
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
-var (
-	version = "dev"
-)
-
 func main() {
 	var debugMode bool
 
 	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
-	opts := &plugin.ServeOpts{ProviderFunc: provider.New(version)}
+	opts := &plugin.ServeOpts{ProviderFunc: provider.Provider}
 
 	if debugMode {
 		err := plugin.Debug(context.Background(), "registry.terraform.io/harvester/harvester", opts)
