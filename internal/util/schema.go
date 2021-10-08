f7b14a1b72ac6066976daac84f8703d0abc29c48
futuretea <Hang.Yu@suse.com>
2021-10-08 09:33:05 +0800

    Fix name validation error msg

--- internal/util/schema.go
+++ internal/util/schema.go
@@ -54,7 +54,7 @@ func IsValidName(i interface{}, k string) ([]string, []error) {
 	}
 
 	if errs := validation.IsDNS1123Subdomain(v); len(errs) > 0 {
-		return nil, []error{fmt.Errorf("expected %q to not be an kubernetes qualified name", k)}
+		return nil, []error{fmt.Errorf("expected %q to not be an kubernetes valid name", k)}
 	}
 
 	return nil, nil
