749abed9ab736626ff447c5cede6e047b2c855f0
futuretea <Hang.Yu@suse.com>
2022-02-24 10:32:23 +0800

    Bump version to 0.3.0

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     harvester = {
-      source  = "registry.terraform.io/harvester/harvester"
-      version = "~> 0.2.9"
+      source  = "harvester/harvester"
+      version = "0.3.0"
     }
   }
 }
