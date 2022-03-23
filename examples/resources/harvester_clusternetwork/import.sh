876482ad327a286664da9ad21cd875708aca356d
futuretea <Hang.Yu@suse.com>
2022-03-23 11:25:23 +0800

    Add clusternetwork import note
    Signed-off-by: futuretea <Hang.Yu@suse.com>

--- examples/resources/harvester_clusternetwork/import.sh
+++ examples/resources/harvester_clusternetwork/import.sh
@@ -1,1 +1,4 @@
-terraform import harvester_clusternetwork.foo <Name>
+# There is a vlan clusternetwork in Harvester clusters by default
+# Import it before manage it
+# And don't destroy it after imported
+terraform import harvester_clusternetwork.vlan vlan
