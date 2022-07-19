97c3089fd9a16e0acde71dd88f9a1920f077052f
futuretea <Hang.Yu@suse.com>
2022-07-19 23:10:51 +0800

    Update examples
    Signed-off-by: futuretea <Hang.Yu@suse.com>

--- examples/data-sources/harvester_image/data-source.tf
+++ examples/data-sources/harvester_image/data-source.tf
@@ -2,3 +2,8 @@ data "harvester_image" "ubuntu20" {
   name      = "ubuntu20"
   namespace = "harvester-public"
 }
+
+data "harvester_image" "opensuse" {
+  namespace    = "harvester-public"
+  display_name = "openSUSE-Leap-42.1-OpenStack.x86_64.qcow2"
+}
