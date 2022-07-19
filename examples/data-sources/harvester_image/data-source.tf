cab82e59be9c9780278ff6c73b443031c495967e
futuretea <Hang.Yu@suse.com>
2022-07-19 23:10:51 +0800

    Update examples
    Signed-off-by: futuretea <Hang.Yu@suse.com>

--- examples/data-sources/harvester_image/data-source.tf
+++ examples/data-sources/harvester_image/data-source.tf
@@ -2,3 +2,8 @@ data "harvester_image" "ubuntu20" {
   name      = "ubuntu20"
   namespace = "harvester-public"
 }
+
+data "harvester_image" "ubuntu1804" {
+  namespace    = "harvester-public"
+  display_name = "ubuntu-18.04-minimal-cloudimg-amd64.img"
+}
