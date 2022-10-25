a5715978e31e66c5507779d66dfb2bea5fc566a0
futuretea <Hang.Yu@suse.com>
2022-10-25 16:15:57 +0800

    Harvester v1.0 is not supported

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,9 +2,12 @@ package provider
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/harvester/terraform-provider-harvester/internal/provider/clusternetwork"
 	"github.com/harvester/terraform-provider-harvester/internal/provider/image"
@@ -67,6 +70,17 @@ func configure(p *schema.Provider) schema.ConfigureContextFunc {
 		if err != nil {
 			return nil, diag.FromErr(err)
 		}
+
+		// check harvester version from settings
+		serverVersion, err := c.HarvesterClient.HarvesterhciV1beta1().Settings().Get(context.Background(), "server-version", metav1.GetOptions{})
+		if err != nil {
+			return nil, diag.FromErr(err)
+		}
+		// harvester version v1.0-head, v1.0.2, v1.0.3 is not supported
+		if strings.HasPrefix(serverVersion.Value, "v1.0") {
+			return nil, diag.FromErr(fmt.Errorf("current Harvester server version is %s, the minimum supported version is v1.1.0", serverVersion.Value))
+		}
+
 		return c, nil
 	}
 }
