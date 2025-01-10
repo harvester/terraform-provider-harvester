f20cab9d7f90dc53e7aa3fce915b3df38ca6f7b8
Moritz Röhrich <moritz.rohrich@suse.com>
2025-01-10 11:40:28 +0100

    Kubeconfig as Base64 string: document risks
    Document the risk of leaking sensitive information via the Terraform
    state files when supplying the kubeconfig as base64 encoded string.
    
    Signed-off-by: Moritz Röhrich <moritz.rohrich@suse.com>

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -11,7 +11,12 @@ terraform {
 provider "harvester" {
   # Path to kubeconfig file
   kubeconfig = "/path/to/kubeconfig.yaml"
-  # alternatively the base64 encoded contents of the kubeconfig file:
+
+  # Alternatively the base64 encoded contents of the kubeconfig file.
+  # CAUTION: When supplying the kubeconfig as base64 encoded string, the
+  # content will be preserved in the Terraform state files in the clear.
+  # Take appropriate measures to avoid leaking sensitive information.
+  #
   # kubeconfig = "YXBpVmVyc2lvb...xvY2FsIgo="
 
   kubecontext = "mycontext"
