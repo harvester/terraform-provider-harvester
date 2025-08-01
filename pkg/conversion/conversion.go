8d28d9bdaac62c42cbe4aaf4495277a71dad3239
Moritz Röhrich <moritz.rohrich@suse.com>
2025-08-01 14:54:08 +0200

    use constants from math package
    Signed-off-by: Moritz Röhrich <moritz.rohrich@suse.com>

--- pkg/conversion/conversion.go
+++ pkg/conversion/conversion.go
@@ -1,31 +1,26 @@
 package conversion
 
-import "fmt"
-
-const (
-	MinInt32  = -(1 << 32)
-	MaxInt32  = 1<<31 - 1
-	MinUint   = 0
-	MinUint32 = 0
-	MaxUint32 = 1<<32 - 1
+import (
+	"fmt"
+	"math"
 )
 
 func IntToUint32(v int) (uint32, error) {
-	if v >= MinUint32 && v <= MaxUint32 {
+	if v >= 0 && v <= math.MaxUint32 {
 		return uint32(v), nil //nolint:gosec
 	}
 	return 0, fmt.Errorf("overflow during typecast from int to uint32")
 }
 
 func IntToUint(v int) (uint, error) {
-	if v >= MinUint {
+	if v >= 0 && v <= math.MaxUint {
 		return uint(v), nil //nolint:gosec
 	}
 	return 0, fmt.Errorf("overflow during typecast from int to uint")
 }
 
 func IntToInt32(v int) (int32, error) {
-	if v >= MinInt32 && v <= MaxInt32 {
+	if v >= math.MinInt32 && v <= math.MaxInt32 {
 		return int32(v), nil //nolint:gosec
 	}
 	return 0, fmt.Errorf("overflow during typecast from int to uint")
