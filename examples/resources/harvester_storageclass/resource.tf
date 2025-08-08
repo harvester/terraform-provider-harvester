92511037282304d17abcbeba607cf5b79bd80a1a
PoAn Yang <poan.yang@suse.com>
2025-08-08 14:56:44 +0800

    feat: add harvester_storageclass example with allowed_topologies
    Signed-off-by: PoAn Yang <poan.yang@suse.com>

--- examples/resources/harvester_storageclass/resource.tf
+++ examples/resources/harvester_storageclass/resource.tf
@@ -18,3 +18,20 @@ resource "harvester_storageclass" "ssd-replicas-3" {
     "diskSelector"        = "ssd,nvme"
   }
 }
+
+resource "harvester_storageclass" "lvm" {
+  name = "lvm"
+
+  volume_provisioner = "lvm.driver.harvesterhci.io"
+  parameters = {
+    "type"   = "striped"
+    "vgName" = "test-vol"
+  }
+
+  allowed_topologies {
+    match_label_expressions {
+      key    = "topology.lvm.csi/node"
+      values = ["harvester-node-0"]
+    }
+  }
+}
