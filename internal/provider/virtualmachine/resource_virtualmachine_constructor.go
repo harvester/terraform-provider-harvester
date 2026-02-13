448c139e06cd3d728050d0117186f13eeea1a8b8
Terraform Provider Developer <terraform@harvester.local>
2026-02-13 17:38:40 +0100

    fix(cdrom-eject): address Copilot review comments
    - Add description to eject field in data source docs
    - Validate that eject is only set on cd-rom type disks, return
      clear error instead of silently ignoring
    - Add test case for CD-ROM with no Tray field set (default behavior)
    
    Signed-off-by: Terraform Provider Developer <terraform@harvester.local>

--- internal/provider/virtualmachine/resource_virtualmachine_constructor.go
+++ internal/provider/virtualmachine/resource_virtualmachine_constructor.go
@@ -254,6 +254,11 @@ func (c *Constructor) Setup() util.Processors {
 					}
 				}
 
+				eject := r[constants.FieldDiskEject].(bool)
+				if eject && !isCDRom {
+					return fmt.Errorf("eject can only be set on cd-rom type disks, but disk %q has type %q", diskName, diskType)
+				}
+
 				vmBuilder.Disk(diskName, diskBus, isCDRom, uint(bootOrder)) // nolint: gosec
 				if diskCacheMode != "" {
 					mode := kubevirtv1.DriverCache(diskCacheMode)
@@ -263,7 +268,7 @@ func (c *Constructor) Setup() util.Processors {
 					}
 				}
 
-				if eject := r[constants.FieldDiskEject].(bool); eject && isCDRom {
+				if eject && isCDRom {
 					disks := vmBuilder.VirtualMachine.Spec.Template.Spec.Domain.Devices.Disks
 					for idx, d := range disks {
 						if d.Name == diskName && d.CDRom != nil {
