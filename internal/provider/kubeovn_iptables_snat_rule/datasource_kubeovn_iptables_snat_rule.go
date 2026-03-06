4632700959d4d144ce12f75809d6b0c461e08f57
Terraform Provider Developer <terraform@harvester.local>
2026-03-06 12:39:41 +0100

    feat(kubeovn): add NAT Gateway resources (Phase 2)
    Add 5 KubeOVN NAT Gateway resources for external connectivity from VPCs:
    - harvester_kubeovn_vpc_nat_gateway: VPC NAT Gateway with delete retry
    - harvester_kubeovn_iptables_eip: Elastic IP with ForceNew on nat_gw_dp
    - harvester_kubeovn_iptables_snat_rule: Source NAT rule
    - harvester_kubeovn_iptables_dnat_rule: Destination NAT rule with protocol
    - harvester_kubeovn_iptables_fip_rule: Floating IP rule
    
    Each resource includes: schema, constructor, CRUD, datasource, importer
    with unit tests, and TF examples. All cluster-scoped via
    NonNamespacedSchemaWrap. Status fields prefixed with status_ to avoid
    collisions with spec fields.
    
    Signed-off-by: Terraform Provider Developer <terraform@harvester.local>

---
+++ internal/provider/kubeovn_iptables_snat_rule/datasource_kubeovn_iptables_snat_rule.go
@@ -0,0 +1,38 @@
+package kubeovn_iptables_snat_rule
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/harvester/terraform-provider-harvester/internal/config"
+	"github.com/harvester/terraform-provider-harvester/internal/util"
+	"github.com/harvester/terraform-provider-harvester/pkg/constants"
+	"github.com/harvester/terraform-provider-harvester/pkg/importer"
+)
+
+func DataSourceKubeOVNIptablesSnatRule() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceKubeOVNIptablesSnatRuleRead,
+		Schema:      DataSourceSchema(),
+	}
+}
+
+func dataSourceKubeOVNIptablesSnatRuleRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c, err := meta.(*config.Config).K8sClient()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	name := d.Get(constants.FieldCommonName).(string)
+	obj, err := c.KubeOVNClient.KubeovnV1().IptablesSnatRules().Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	stateGetter, err := importer.ResourceKubeOVNIptablesSnatRuleStateGetter(obj)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	return diag.FromErr(util.ResourceStatesSet(d, stateGetter))
+}
