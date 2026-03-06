c07d6f4a3be4653a60ed9c0752456d6f12e2b3ca
Terraform Provider Developer <terraform@harvester.local>
2026-03-06 14:42:30 +0100

    feat(kubeovn): add OVN NAT, VIP, VPC DNS, and Switch LB Rule resources (Phase 4)
    Add 7 new KubeOVN resources for OVN-level networking:
    - harvester_kubeovn_ovn_eip: OVN Elastic IP
    - harvester_kubeovn_ovn_fip: OVN Floating IP
    - harvester_kubeovn_ovn_snat_rule: OVN SNAT Rule
    - harvester_kubeovn_ovn_dnat_rule: OVN DNAT Rule
    - harvester_kubeovn_vip: Virtual IP
    - harvester_kubeovn_vpc_dns: VPC DNS
    - harvester_kubeovn_switch_lb_rule: Switch LB Rule
    
    Each resource includes schema, constructor, CRUD operations, datasource,
    importer with unit tests, examples, and generated documentation.
    
    Signed-off-by: Terraform Provider Developer <terraform@harvester.local>

---
+++ pkg/importer/resource_kubeovn_switch_lb_rule_importer.go
@@ -0,0 +1,51 @@
+package importer
+
+import (
+	kubeovnv1 "github.com/kubeovn/kube-ovn/pkg/apis/kubeovn/v1"
+
+	"github.com/harvester/terraform-provider-harvester/pkg/constants"
+	"github.com/harvester/terraform-provider-harvester/pkg/helper"
+)
+
+func flattenSlrPorts(ports []kubeovnv1.SlrPort) []map[string]interface{} {
+	result := make([]map[string]interface{}, 0, len(ports))
+	for _, port := range ports {
+		result = append(result, map[string]interface{}{
+			constants.FieldKubeOVNSlrPortName:       port.Name,
+			constants.FieldKubeOVNSlrPortPort:       int(port.Port),
+			constants.FieldKubeOVNSlrPortTargetPort: int(port.TargetPort),
+			constants.FieldKubeOVNSlrPortProtocol:   port.Protocol,
+		})
+	}
+	return result
+}
+
+func ResourceKubeOVNSwitchLBRuleStateGetter(obj *kubeovnv1.SwitchLBRule) (*StateGetter, error) {
+	states := map[string]interface{}{
+		constants.FieldCommonName:                         obj.Name,
+		constants.FieldCommonDescription:                  GetDescriptions(obj.Annotations),
+		constants.FieldCommonTags:                         GetTags(obj.Labels),
+		constants.FieldCommonLabels:                       GetLabels(obj.Labels),
+		constants.FieldKubeOVNSwitchLBRuleVip:             obj.Spec.Vip,
+		constants.FieldKubeOVNSwitchLBRuleNamespace:       obj.Spec.Namespace,
+		constants.FieldKubeOVNSwitchLBRuleSelector:        obj.Spec.Selector,
+		constants.FieldKubeOVNSwitchLBRuleEndpoints:       obj.Spec.Endpoints,
+		constants.FieldKubeOVNSwitchLBRuleSessionAffinity: obj.Spec.SessionAffinity,
+		constants.FieldKubeOVNSwitchLBRulePorts:           flattenSlrPorts(obj.Spec.Ports),
+		constants.FieldKubeOVNSwitchLBRuleStatusPorts:     obj.Status.Ports,
+		constants.FieldKubeOVNSwitchLBRuleStatusService:   obj.Status.Service,
+	}
+
+	if len(obj.Status.Service) > 0 {
+		states[constants.FieldCommonState] = constants.StateCommonReady
+	} else {
+		states[constants.FieldCommonState] = constants.StateCommonActive
+	}
+
+	return &StateGetter{
+		ID:           helper.BuildID("", obj.Name),
+		Name:         obj.Name,
+		ResourceType: constants.ResourceTypeKubeOVNSwitchLBRule,
+		States:       states,
+	}, nil
+}
