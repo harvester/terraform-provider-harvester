3f394b5ff9b63c5dc48a7e2d5e06b1973fbc6ba4
Terraform Provider Developer <terraform@harvester.local>
2026-03-08 14:20:22 +0100

    fix(vm): resolve clock ConflictsWith and DataSourceSchemaWrap issues
    - Remove ConflictsWith from clock timezone/utc_offset_seconds (breaks
      DataSourceSchemaWrap which sets Computed-only fields)
    - Add manual mutual exclusion validation in parseClock() returning error
    - Clear AtLeastOneOf in DataSourceSchemaWrap (fixes pre-existing bug
      with schedule_backup datasource)
    
    Tested on Harvester v1.7.1: create/idempotent/destroy cycle passed.
    
    Signed-off-by: Terraform Provider Developer <terraform@harvester.local>

--- internal/provider/virtualmachine/resource_virtualmachine_constructor.go
+++ internal/provider/virtualmachine/resource_virtualmachine_constructor.go
@@ -495,7 +495,10 @@ func (c *Constructor) Setup() util.Processors {
 			Field: constants.FieldVirtualMachineClock,
 			Parser: func(i interface{}) error {
 				r := i.(map[string]interface{})
-				clock := parseClock(r)
+				clock, err := parseClock(r)
+				if err != nil {
+					return err
+				}
 				vmBuilder.VirtualMachine.Spec.Template.Spec.Domain.Clock = clock
 				return nil
 			},
@@ -582,12 +585,17 @@ func parseHyperv(r map[string]interface{}) *kubevirtv1.FeatureHyperv {
 	return hv
 }
 
-func parseClock(r map[string]interface{}) *kubevirtv1.Clock {
+func parseClock(r map[string]interface{}) (*kubevirtv1.Clock, error) {
 	clock := &kubevirtv1.Clock{}
-	if tz, ok := r[constants.FieldClockTimezone].(string); ok && tz != "" {
+	tz, hasTZ := r[constants.FieldClockTimezone].(string)
+	offset, hasOffset := r[constants.FieldClockUTCOffsetSeconds].(int)
+	if hasTZ && tz != "" && hasOffset && offset != 0 {
+		return nil, errors.New("clock: timezone and utc_offset_seconds are mutually exclusive")
+	}
+	if hasTZ && tz != "" {
 		timezone := kubevirtv1.ClockOffsetTimezone(tz)
 		clock.Timezone = &timezone
-	} else if offset, ok := r[constants.FieldClockUTCOffsetSeconds].(int); ok && offset != 0 {
+	} else if hasOffset && offset != 0 {
 		clock.UTC = &kubevirtv1.ClockOffsetUTC{OffsetSeconds: &offset}
 	}
 
@@ -639,7 +647,7 @@ func parseClock(r map[string]interface{}) *kubevirtv1.Clock {
 
 		clock.Timer = timer
 	}
-	return clock
+	return clock, nil
 }
 
 func Updater(c *client.Client, ctx context.Context, vm *kubevirtv1.VirtualMachine) util.Constructor {
